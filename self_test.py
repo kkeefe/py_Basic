4ba746da6c77283236a2849ff052473b455bfdda
kkeefe <kevinpk@hawaii.edu>
2019-04-18 14:41:35 -1000

    updates to hello.py can make board spit out triggers but extra bytes..

---
+++ self_test.py
@@ -0,0 +1,9 @@
+# let's try converting some int inputs to hex values!!
+
+val = input("input the value of the register you want to write! ")
+
+print(f'the value you entered is: {val}')
+
+hex_val = hex(val)
+
+# print(f'this value in hex format is: {hex_val}')
