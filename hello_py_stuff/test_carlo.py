b792848fa4b31fbd28a941679dfeb0df06a34494
kkeefe <kevinpk@hawaii.edu>
2020-09-15 16:53:43 -0700

    fully function carlo sim on the emacs within windows!! wuttt

--- hello_py_stuff/test_carlo.py
+++ hello_py_stuff/test_carlo.py
@@ -16,28 +16,26 @@ from scipy.stats import kstest
 
 RANDOM_STATE = 31415
 
-## let's do some carlo things
-# print("ni hao fren")
-# dataset = seaborn.load_dataset('titanic')
-# ages = dataset.age.dropna()
+# let's do some carlo things
+ages = range(1,100,1)
 
-# # norm stuff
-# mean, std_div = 3, 2
-# norm_dis = norm(loc = mean, scale = std_div)
-# x = np.linspace(-6, 12, 200)
-# print("values we got for x are:", x)
-# _, ax = plt.subplots(1, 1)
-# ax.plot(x, norm_dis.pdf(x), '-', lw=2)
-# plt.title("bologna")
-# plt.show()
+# norm stuff
+mean, std_div = 3, 2
+norm_dis = norm(loc = mean, scale = std_div)
+x = np.linspace(-6, 12, 200)
+print("values we got for x are:", x)
+_, ax = plt.subplots(1, 1)
+ax.plot(x, norm_dis.pdf(x), '-', lw=2)
+plt.title("bologna")
+plt.show()
 
-# # gamma stuff
-# gamma_distribution = gamma(loc = 3, scale = 3, a = 1)
-# x = np.linspace(0, 12, 200)
-# _, ax = plt.subplots(1, 1)
-# ax.plot(x, gamma_distribution.pdf(x), '-', lw=2)
-# plt.title('Exponential distribution (gamma with a = 1)')
-# plt.show()
+# gamma stuff
+gamma_distribution = gamma(loc = 3, scale = 3, a = 1)
+x = np.linspace(0, 12, 200)
+_, ax = plt.subplots(1, 1)
+ax.plot(x, gamma_distribution.pdf(x), '-', lw=2)
+plt.title('Exponential distribution (gamma with a = 1)')
+plt.show()
 
 def fit_and_plot(dist):
     params = dist.fit(ages)
@@ -52,32 +50,32 @@ def fit_and_plot(dist):
     plt.show()
     return dist, loc, scale, arg
 
-# fit_and_plot(scipy.stats.norm)
+fit_and_plot(scipy.stats.norm)
 
 # monte carlo intro
-# _90_conf_interval = 3.29
-# maintenance = norm(loc = (20 + 10) / 2, scale = (20 - 10) / _90_conf_interval)
-# labor = norm(loc = (8 + -2) / 2, scale = (8 - -2) / _90_conf_interval)
-# raw_material = norm(loc = (9 + 3) / 2, scale = (9 - 3) / _90_conf_interval)
-# prod_level = norm(loc = (35000 + 15000) / 2, scale = (35000 - 15000) / _90_conf_interval)
-# number_of_simulations = 1000000
-# maintenance_results = maintenance.rvs(number_of_simulations)
-# labor_results = labor.rvs(number_of_simulations)
-# raw_materials_results = raw_material.rvs(number_of_simulations)
-# prod_level_results = prod_level.rvs(number_of_simulations)
-# data = pd.DataFrame({
-#     "maintenance_savings_per_unit": maintenance_results,
-#     "labor_savings_per_unit": labor_results,
-#     "raw_materials_savings_per_unit": raw_materials_results,
-#     "production_level": prod_level_results
-# })
-# data["total_savings"] = (data.maintenance_savings_per_unit + data.labor_savings_per_unit + data.raw_materials_savings_per_unit) * data.production_level
+_90_conf_interval = 3.29
+maintenance = norm(loc = (20 + 10) / 2, scale = (20 - 10) / _90_conf_interval)
+labor = norm(loc = (8 + -2) / 2, scale = (8 - -2) / _90_conf_interval)
+raw_material = norm(loc = (9 + 3) / 2, scale = (9 - 3) / _90_conf_interval)
+prod_level = norm(loc = (35000 + 15000) / 2, scale = (35000 - 15000) / _90_conf_interval)
+number_of_simulations = 1000000
+maintenance_results = maintenance.rvs(number_of_simulations)
+labor_results = labor.rvs(number_of_simulations)
+raw_materials_results = raw_material.rvs(number_of_simulations)
+prod_level_results = prod_level.rvs(number_of_simulations)
+data = pd.DataFrame({
+    "maintenance_savings_per_unit": maintenance_results,
+    "labor_savings_per_unit": labor_results,
+    "raw_materials_savings_per_unit": raw_materials_results,
+    "production_level": prod_level_results
+})
+data["total_savings"] = (data.maintenance_savings_per_unit + data.labor_savings_per_unit + data.raw_materials_savings_per_unit) * data.production_level
 
-# plt.hist(data.total_savings, bins = 100)
-# plt.axvline(x = 400000, c = "r")
-# plt.show()
+plt.hist(data.total_savings, bins = 100)
+plt.axvline(x = 400000, c = "r")
+plt.show()
 
-# print(data[data["total_savings"] < 400000].count()["total_savings"] / 1000000)
+print(data[data["total_savings"] < 400000].count()["total_savings"] / 1000000)
 
 # monte carlo 2
 avg = 1
